9420b92f5d77a0b63e01f83ca92b07629ea6a0ab
eleftheros <eleftheros_all@protonmail.com>
2022-01-03 14:04:01 +0200

    Add Button shared component

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,18 @@
 import Card from "./shared/Card";
+import { FaEdit } from "react-icons/fa";
+import Button from "./shared/Button";
 
 const Todo = ({ title, isCompleted }) => {
   return (
-    <li className="bg-gray-300 rounded">
-      <Card>{title}</Card>
+    <li className="bg-gray-300 rounded relative my-4 p-4">
+      <Card>
+        <h2 className="text-center font-bold text-2xl">{title}</h2>
+        <div className="absolute top-0 left-3/4">
+          <Button>
+            <FaEdit color="orange" />
+          </Button>
+        </div>
+      </Card>
     </li>
   );
 };
